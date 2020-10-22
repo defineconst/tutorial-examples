22d622283f8dbecce6451ad4c437b434e945a9bc
gz65555@gmail.com <gz65555@gmail.com>
2020-10-22 11:39:14 +0800

    feat: add async await support

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as o3 from "oasis-engine";
+import "regenerator-runtime";
 
 const engine = new o3.WebGLEngine("canvas");
 engine.canvas.resizeByClientSize();
@@ -7,5 +8,20 @@ const root = engine.sceneManager.activeScene.createRootEntity();
 const cameraEntity = root.createChild("camera");
 const camera = cameraEntity.addComponent(o3.Camera);
 camera.backgroundColor = new o3.Vector4(0.2, 0.2, 0.2, 1);
+
+function wait() {
+	return new Promise((resolve) => {
+    setTimeout(()=>{
+      resolve()
+    }, 3000)
+  });
+}
+
+async function test(abc: string) {
+  await wait();
+  console.log(abc)
+}
+
+test("abc");
 
 engine.run();
